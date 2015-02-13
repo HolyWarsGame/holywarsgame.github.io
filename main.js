cf512c96b15a2302641072be92a286053ce4a0a9
CursedAnubis <cursedanubis@gmail.com>
2015-02-13 09:48:48 -0800

    update

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-var souls = 0;
+var souls = 50000;
 
 function demonClick(number){
     souls = souls + number;
@@ -21,8 +21,22 @@ function buyWarrior(){
     document.getElementById('WarriorCost').innerHTML = nextCost;  //updates the cursor cost for the user
 };
 
+var weapons = 0;
+
+function buyWeapon(){
+    var WeaponCost = Math.floor(50 * Math.pow(1.1,weapons));     //works out the cost of this weapon
+    if(souls >= WeaponCost){                                   //checks that the player can afford the weapon
+        weapons = weapons + 1;                                   //increases number of weapons
+    	souls = souls - WeaponCost;                          //removes the souls spent
+        document.getElementById('weapons').innerHTML = weapons;  //updates the number of weapons for the user
+        document.getElementById('souls').innerHTML = souls;  //updates the number of souls for the user
+    };
+    var nextWeapCost = Math.floor(50 * Math.pow(1.1,weapons));       //works out the cost of the next weapon
+    document.getElementById('WeaponCost').innerHTML = nextWeapCost;  //updates the cursor cost for the user
+};
+
 window.setInterval(function(){
 	
 	demonClick(warriors);
 	
-}, 10000);
+}, (10000-50*weapons));
