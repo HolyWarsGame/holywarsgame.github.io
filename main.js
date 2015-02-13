2e0550f126b9a4de34f9370a4a5f06fb95ddd530
CursedAnubis <cursedanubis@gmail.com>
2015-02-13 15:28:38 -0800

    Test Split into more modular files

--- main.js
+++ main.js
@@ -43,18 +43,6 @@ function buyPeasant(){
     document.getElementById('PeasantCost').innerHTML = nextPeasantCost;  //updates the Peasant cost for the user
 };
 
-var taverns = 0;
-function buyTavern(){
-	var TavernCost = Math.floor(1000 * Math.pow(1.5,taverns));
-		if(gold >= TavernCost){
-			taverns = taverns + 1;
-			gold = gold - TavernCost;
-			document.getElementById('taverns').innerHTML = taverns;
-			document.getElementById('gold').innerHTML = gold;
-		};
-		var nextTavernCost = Math.floor(1000 * Math.pow(1.5, taverns));
-		document.getElementByID('TavernCost').innerHTML = nextTavernCost;
-};
 
 var priests = 0;
 function buyPriest(){
@@ -106,14 +94,12 @@ window.setInterval(function(){
 window.setInterval(function(){
 
 	goldClick(peasants);
+	
 }, 1000);
 
 window.setInterval(function(){
 
 	clickFaith(priests*0.1);
+	
 }, 1000);
 
-window.setInterval(function(){
-
-	peasantClick(taverns);
-}, 10000);
