afdc40b58f4d93e04d5267db02f60a0a722e97bd
CursedAnubis <cursedanubis@gmail.com>
2015-02-18 10:38:15 -0800

    Mines Bought button edit

--- scripts/saveload.js
+++ scripts/saveload.js
@@ -57,6 +57,7 @@
 			if(myBool = true){
 				minesOpened = true
 				document.getElementById("Mining").style.display = "block";
+				document.getElementById('btnbuyMiner').innerHTML = "Mines Bought");
 			}
 		}			
 		if($.cookie("tavernpeasants") != null){
