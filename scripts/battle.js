19a59e91f009b2a2004a81d017d666ef5cb19fe2
CursedAnubis <cursedanubis@gmail.com>
2015-02-19 12:26:23 -0800

    Alternate solution for animated progress bar

--- scripts/battle.js
+++ scripts/battle.js
@@ -11,19 +11,37 @@ function battleOgre(number){
 	document.getElementById('BatOgreProgBarBox').style.display = "block";	
 	var progressBar = document.getElementById('BatOgreProgBar');
 
-	var go = setInterval(function() {
-		if ( percentComplete == 100) {
-			clearInterval(go);
-			progressBar.style.display = "none";
-		} else {
-			percentComplete = percentComplete + 1;
-//			console.log(percentComplete);
-		}
-		progressBar.setAttribute("aria-valuenow", percentComplete);
-		console.log(progressBar.get);
-		progressBar.style.width= percentComplete+'%'+';';
-//		console.log(progress);
-	}, 500);
+	// var go = setInterval(function() {
+		// if ( percentComplete == 100) {
+			// clearInterval(go);
+			// progressBar.style.display = "none";
+		// } else {
+			// percentComplete = percentComplete + 1;
+		// }
+		// progressBar.setAttribute("aria-valuenow", percentComplete);
+		// console.log(progressBar.get);
+		// progressBar.style.width= percentComplete+'%'+';';
+	// }, 500);
+	$('.progress-bar').each(function() {
+  
+  	var $bar = $(this);
+    var progress = setInterval(function() {
+      
+      var currWidth = parseInt($bar.attr('aria-valuenow'));
+      var maxWidth = parseInt($bar.attr('aria-valuemax'));
+      
+	  //update the progress
+        $bar.width(currWidth+'%');
+        $bar.attr('aria-valuenow',currWidth+10);
+      
+      //clear timer when max is reach
+      if (currWidth >= maxWidth){
+        clearInterval(progress);
+      }
+      
+    }, 500);
+  
+	});
 };
 
 //function incrementProgBar(elem, percentComplete){
