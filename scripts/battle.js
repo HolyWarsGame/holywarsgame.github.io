06090ba82e3f031099b618615bfa239190ce154a
CursedAnubis <cursedanubis@gmail.com>
2015-02-19 12:02:27 -0800

    Different approach to updating bar

--- scripts/battle.js
+++ scripts/battle.js
@@ -20,6 +20,7 @@ function battleOgre(number){
 			console.log(percentComplete);
 		}
 		progressBar.setAttribute("aria-valuenow", percentComplete);
+		progressBar.style.width = "min-width: 2em; width: " + percentComplete + "%;";
 //		console.log(progress);
 	}, 500);
 };
